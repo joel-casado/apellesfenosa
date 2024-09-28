a9c29f15df6a374be782d8ee4956f2ee0f97c590
unknown <adrian.pena@inslapineda.cat>
2024-09-28 18:43:11 +0200

    Ver obras + css

--- views/obras/obras.php
+++ views/obras/obras.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Obras</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/obras/obras.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
     <h1>Listado de Obras</h1>
@@ -11,9 +14,10 @@
         <thead>
             <tr>
                 <th>ID</th>
-                <th>Nombre</th>
+                <th>Nom</th>
                 <th>autor</th>
-                <th>Descripción</th>
+                <th>Descripció</th>
+                <th>Acció</th>
             </tr>
         </thead>
         <tbody>
@@ -23,6 +27,7 @@
                     <td><?php echo $obra["titulo"]; ?></td>
                     <td><?php echo $obra['autor']; ?></td>
                     <td><?php echo $obra['descripcion']; ?></td>
+                    <td><a href="editar.php?id=<?php echo $obra['numero_registro']; ?>" class="edit-button">Editar</a></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
