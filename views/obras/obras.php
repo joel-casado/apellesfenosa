7b0119a2d100d65c2014c625c39758bd0a0ed2d8
unknown <sergicruga@gmail.com>
2024-10-04 09:08:17 +0200

    un comentario

--- views/obras/obras.php
+++ views/obras/obras.php
@@ -9,14 +9,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
+    <div class="header">
+        <img src="images/login/logo.png" alt="Museu Apel·les Fenosa">
+    </div>
+
     <h1>Listado de Obras</h1>
     <table border="1">
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Nom</th>
-                <th>autor</th>
-                <th>Descripció</th>
+                <th></th>
+                <th>Número Registre</th>
+                <th>Nom Objecte</th>
+                <th>Títol</th>
+                <th>Autor</th>
+                <th>Datació</th>
+                <th>Ubicació</th>
                 <th>Acció</th>
             </tr>
         </thead>
@@ -24,9 +31,11 @@
             <?php foreach ($obras as $obra): ?>
                 <tr>
                     <td><?php echo $obra["numero_registro"]; ?></td>
-                    <td><?php echo $obra["titulo"]; ?></td>
+                    <td><?php echo $obra["Nom Objecte"]; ?></td>
                     <td><?php echo $obra['autor']; ?></td>
-                    <td><?php echo $obra['descripcion']; ?></td>
+                    <td><?php echo $obra['datacio']; ?></td>
+                    <td><?php echo $obra['ubicacio']; ?></td>
+                    <td><?php echo $obra['accio']; ?></td>
                     <td><a href="editar.php?id=<?php echo $obra['numero_registro']; ?>" class="edit-button">Editar</a></td>
                 </tr>
             <?php endforeach; ?>
