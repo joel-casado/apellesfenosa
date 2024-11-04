90f3a18b9cc3e4ba88dbdfe7a468be3968594176
joel-casado <joel.casado@inslapineda.cat>
2024-11-04 20:15:01 +0100

    Sergio reeintenta

--- controllers/ExposicionesController.php
+++ controllers/ExposicionesController.php
@@ -29,7 +29,7 @@ class ExposicionesController {
             exit();
         }
     }
-    
+    //Md reactualiza
 
     public function crea_expo() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
