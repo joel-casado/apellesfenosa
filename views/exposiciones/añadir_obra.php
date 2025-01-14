6861f43a79444c3eaeafd6a4a3c36736b29b97b5
joel-casado <joel.casado@inslapineda.cat>
2025-01-14 21:55:06 +0100

    obras expo bla bla

--- views/exposiciones/añadir_obra.php
+++ views/exposiciones/añadir_obra.php
@@ -5,16 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Obras</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles/exposiciones/anadirObra.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="styles/exposiciones/añadir_obra.css">
 </head>
-    
 <body>
-    <div class="header">
-        <img src="images/login/logo.png" alt="Museu Apel·les Fenosa">
-    </div>
+    <a href="index.php?controller=Exposiciones&action=listado_exposiciones" class="edit-button">Tornar</a>
     <h1>Llistat d'obres</h1>
-    <form method="POST" action="index.php?controller=Exposiciones&action=anadirObra&id_exposicion=<?php echo $id_exposicion; ?>">
+    <div class="search-bar-container">
+        <div class="crear-usuario">
+            <button type="submit" form="obraForm" class="btn btn-success">Afegir</button>
+        </div>
+    </div>
+    <form id="obraForm" method="POST" action="index.php?controller=Exposiciones&action=anadirObra&id_exposicion=<?php echo $id_exposicion; ?>">
         <table>
             <thead>
                 <tr>
@@ -34,23 +36,21 @@
                                 <input type="checkbox" class="checkbox-obra" name="exposicion_ids[]" value="<?php echo $obra['numero_registro']; ?>">
                             </td>
                             <td>
-                               <!-- <img src="" style="max-width: 100px; height: auto;">-->
+                                <!-- <img src="" style="max-width: 100px; height: auto;">-->
                             </td>
-                            <td><?php echo $obra["numero_registro"]; ?></td>
-                            <td><?php echo $obra["nombre_objeto"]; ?></td>
+                            <td><?php echo $obra['numero_registro']; ?></td>
+                            <td><?php echo $obra['nombre_objeto']; ?></td>
                             <td><?php echo $obra['titulo']; ?></td>
                             <td><?php echo $obra['ubicacion']; ?></td>
                         </tr>
                     <?php endforeach; ?>
                 <?php else: ?>
                     <tr>
-                        <td colspan="9">No hay obras disponibles para esta exposición.</td>
+                        <td colspan="6">No hi ha obres disponibles per a la exposició.</td>
                     </tr>
                 <?php endif; ?>
             </tbody>
         </table>
-        <button type="submit" class="btn btn-success">Afegir</button>
     </form>
-
 </body>
 </html>
--- views/exposiciones/ver_obras.php
+++ views/exposiciones/ver_obras.php
@@ -5,11 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Obras</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles/exposiciones/ver_obras.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="styles/exposiciones/ver_obras.css">
 </head>
 <body>
-    
     <div class="container">
         <a href="index.php?controller=Exposiciones&action=listado_exposiciones" class="edit-button">Tornar</a>
         <h1>Exposició seleccionada</h1>
@@ -19,13 +18,8 @@
                 return;
             }
         ?>
-        <div class="search-bar-container">
-                <form class="search-bar">
-                    <input type="text" id="q" placeholder="Cerca d'obres" onkeyup="search()">
-                </form>
-            <div class="crear-usuario">
-                <a href="index.php?controller=Exposiciones&action=anadirObra&id_exposicion=<?php echo $id_exposicion; ?>" class="btn btn-success">Afegir</a>
-            </div>
+        <div class="crear-usuario">
+            <a href="index.php?controller=Exposiciones&action=anadirObra&id_exposicion=<?php echo $id_exposicion; ?>" class="btn btn-success">Afegir</a>
         </div>
         <div class="usuaris_box">
             <table>
@@ -36,6 +30,7 @@
                         <th>Nom Objecte</th>
                         <th>Títol</th>
                         <th>Ubicació</th>
+                        <th>Acció</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -47,17 +42,19 @@
                                 <td><?php echo $obra["nombre_objeto"]; ?></td>
                                 <td><?php echo $obra['titulo']; ?></td>
                                 <td><?php echo $obra['ubicacion']; ?></td>
+                                <td>
+                                    <a href="index.php?controller=Exposiciones&action=removeObra&id_exposicion=<?php echo $id_exposicion; ?>&numero_registro=<?php echo $obra['numero_registro']; ?>" class="btn btn-danger"><i class="fas fa-times"></i></a>
+                                </td>
                             </tr>
                         <?php endforeach; ?>
                     <?php else: ?>
                         <tr>
-                            <td colspan="5">No hay obras disponibles para esta exposición.</td>
+                            <td colspan="6">No hay obras disponibles para esta exposición.</td>
                         </tr>
                     <?php endif; ?>
                 </tbody>
             </table>
         </div>
     </div>
-    <script src="scripts/busqueda.js"></script>
 </body>
 </html>
